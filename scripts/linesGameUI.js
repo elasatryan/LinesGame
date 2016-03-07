8ae6cd17d79e4d9e4361844793e0d7a0a1dcb4f8
artbab <artbab95@gmail.com>
2016-03-07 16:12:04 +0400

    added new themes

--- scripts/linesGameUI.js
+++ scripts/linesGameUI.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     var colors = ['red', 'blue', 'green', 'yellow', 'pink', 'cyan', 'purple'],
-        themes = ['red', 'pink', 'teal'],
+        themes = ['red','pink','purple','deep-purple','indigo','blue','light-blue','cyan','teal','green','light-green','lime','yellow','amber','orange','deep-orange','brown','grey','blue-grey'],
         gameActions = {
             add: function (element, colorName) {
                 element.addClass(colorName);
